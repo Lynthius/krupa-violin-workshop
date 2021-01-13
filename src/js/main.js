a9590c25145525acd4bd09eff6b59ed69aff71b9
lynthius <t.s.przyborowski@gmail.com>
2021-01-13 18:17:49 +0100

    create nav menu open and close func

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
-
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".navigation");
-
+const body = document.querySelector("body");
 
 // Hamburger menu handler and social links in mobile version
 function handleClick() {
@@ -11,5 +10,14 @@ function handleClick() {
     hamburger.classList.contains("hamburger--active")
   );
   nav.classList.toggle("navigation--active");
+  disableScroll();
 }
 hamburger.addEventListener("click", handleClick);
+
+function disableScroll() {
+  if (hamburger.classList.contains("hamburger--active")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "unset";
+  }
+}
