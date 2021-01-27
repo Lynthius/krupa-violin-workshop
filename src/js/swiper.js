3ce077c005d971ff9e2e5d6bfab8ab866d19b03d
lynthius <t.s.przyborowski@gmail.com>
2021-01-27 21:06:53 +0100

    add new styles to hero slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,22 @@
 var appendNumber = 4;
 var prependNumber = 1;
 
-var swiperHero = new Swiper(".swiper-container__hero", {
+const swiperHero = new Swiper(".swiper-container__hero", {
   centeredSlides: true,
-  // slidesPerView: 1,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 1000,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
   spaceBetween: 0,
   pagination: {
     el: ".swiper-pagination__hero",
-    dynamicBullets: true,
+    // dynamicBullets: true,
+    clickable: true,
   },
   loop: true,
 });
@@ -35,38 +44,38 @@ const swiperGallery = new Swiper(".swiper-container__gallery", {
   },
 });
 
-document
-  .querySelector(".prepend-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper1.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-    ]);
-  });
+// document
+//   .querySelector(".prepend-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper1.prependSlide([
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+//     ]);
+//   });
 
-document
-  .querySelector(".prepend-slide")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper1.prependSlide(
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-    );
-  });
+// document
+//   .querySelector(".prepend-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper1.prependSlide(
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+//     );
+//   });
 
-document.querySelector(".append-slide").addEventListener("click", function (e) {
-  e.preventDefault();
-  swiper1.appendSlide(
-    '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-  );
-});
+// document.querySelector(".append-slide").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   swiper1.appendSlide(
+//     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+//   );
+// });
 
-document
-  .querySelector(".append-2-slides")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    swiper1.appendSlide([
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-    ]);
-  });
+// document
+//   .querySelector(".append-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper1.appendSlide([
+//       '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+//       '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+//     ]);
+//   });
