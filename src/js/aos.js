ea04a33526b15fc781e1d3eb9d8036739b7ce380
lynthius <t.s.przyborowski@gmail.com>
2021-02-15 21:32:54 +0100

    add rellax library and animations

--- src/js/aos.js
+++ src/js/aos.js
@@ -11,8 +11,8 @@ AOS.init({
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 280, // offset (in px) from the original trigger point
-  delay: 100, // values from 0 to 3000, with step 50ms
+  offset: 220, // offset (in px) from the original trigger point
+  delay: 50, // values from 0 to 3000, with step 50ms
   duration: 500, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
